69c1097de597c80801f674a553bf13b12f25155f
Lucas Borges da Silva <lucas18sborges@gmail.com>
2022-10-21 17:38:14 -0300

    Atualização funcional

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, send_file, request
+from logic.download import Download
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/download', methods=['GET', 'POST'])
+def download():
+    url = request.form['pesquisa']
+    d = Download(url)
+    return send_file(d.download(), as_attachment=True)
 
 
 if __name__ == '__main__':
--- logic/download.py
+++ logic/download.py
@@ -1,25 +1,35 @@
 from pytube import YouTube
+from pytube import Search
 import os
-import glob
+from glob import glob
 
 
 class Download:
-    def __init__(self, url):
-        self.url = url
-        self.yt = YouTube(url)
-        self.stream = self.yt.streams.filter(
-            only_audio=True, file_extension="mp3").first()
-
-    def download(self):
-        self.stream.download('/temp')
-        return self.stream.default_filename
+    def __init__(self, find=None):
+        self.text = find
+        self.search = Search(self.text)
+        self.yt = YouTube('https://www.youtube.com/watch?v=' +
+                          getattr(self.search.results[0], 'video_id'))
+        self.streams = self.yt.streams.get_audio_only()
+        for file in glob('temp/*'):
+            os.remove(file)
 
     def get_title(self):
         return self.yt.title
 
-    def get_author(self):
-        return self.yt.author
+    def download(self):
+        try:
+            out_file = self.streams.download('temp/')
 
-    def del_video(self):
-        os.remove('/temp/' + self.stream.default_filename + 'mp3')
-        return True
+            base, ext = os.path.splitext(out_file)
+            new_file = base + '.mp3'
+            os.rename(out_file, new_file)
+            print(new_file)
+            return new_file
+        except AttributeError:
+            return False
+
+
+if __name__ == '__main__':
+    d = Download(find=input('Enter search: '))
+    d.download()
